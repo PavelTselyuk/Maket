1837cc0473d6a47ea9093b696f4e20b807bfa120
Pavel Tselyuk <pavlick_astap@mail.ru>
2020-07-12 17:03:47 +0300

    modal window animation off

--- client/src/components/LoginForm/index.js
+++ client/src/components/LoginForm/index.js
@@ -12,16 +12,10 @@ class LoginForm extends Component {
             <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
                Запустить модальное окно
             </button>
-            <div className="modal fade" id="exampleModal" tabIndex="-1" role="dialog"
+            <div className="modal" id="exampleModal" tabIndex="-1" role="dialog"
                aria-labelledby="exampleModal" aria-hidden="true">
                <div className="modal-dialog modal-dialog-centered" role="document">
                   <div className="modal-content modal_window">
-                     {/* <div className="modal-header">
-                        <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
-                        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                           <span aria-hidden="true">&times;</span>
-                        </button>
-                     </div> */}
                      <div className="modal-body">
                         <p className="top_text text-center">Log in to collaborate on "Schools"</p>
                         <span className="col-1"></span>
@@ -32,10 +26,6 @@ class LoginForm extends Component {
                         <InputGroup />
                         <AdditionalLinks />
                      </div>
-                     {/* <div className="modal-footer">
-                        <button type="button" className="btn btn-primary">Save changes</button>
-                        <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-                     </div> */}
                   </div>
                </div>
             </div>
