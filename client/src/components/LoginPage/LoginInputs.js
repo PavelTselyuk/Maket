fd9e2e7975e931ee9573a721832a7a839220c5c7
Pavel Tselyuk <pavlick_astap@mail.ru>
2020-07-13 03:48:42 +0300

    eye icon for password input

--- client/src/components/LoginPage/LoginInputs.js
+++ client/src/components/LoginPage/LoginInputs.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './loginPage.css'
 
 function LoginInputs() {
-   return (
+   let [visibility, setVisibility] = useState(false);
 
+   return (
       <div className="col">
          <div className="row mb-4 mx-0">
             <h3>Войдите в свой аккаунт</h3>
@@ -19,10 +20,15 @@ function LoginInputs() {
 
             <div className="form-row justify-content-between fullWidth">
                <span className="above_inputs">Пароль</span>
-               <a className="above_inputs" href="">Забыли пароль</a>
+               <a className="above_inputs" href="">Забыли пароль?</a>
             </div>
-            <div className="form-row">
-               <input className="col-12 mb-4 px-3 form-control loginPage_input" id="password" type="password" size="60" placeholder="Password" />
+            <div className="form-row mb-4 px-3  with_icon">
+               <input className="col-11 form-control loginPage_input password_input" id="password" type={visibility ? "text" : "password"} size="60" placeholder="Password" />
+               {visibility ?
+                  <i className="material-icons" onClick={() => setVisibility(!visibility)}>visibility</i>
+                  :
+                  <i className="material-icons" onClick={() => setVisibility(!visibility)}>visibility_off</i>
+               }
             </div>
          </form>
          <div className="row mt-2 mx-0">
